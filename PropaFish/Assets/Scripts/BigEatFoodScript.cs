7d61865f78e321dc87185dbfcdf8e94c8f22e3b3
Dorathor <dario.rahimic@hotmail.com>
2018-04-14 16:42:15 +0200

    Lots of stuff, Big fish, images, materials

---
+++ PropaFish/Assets/Scripts/BigEatFoodScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigEatFoodScript : MonoBehaviour {
+
+    public bool isEating = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+        //If it colllides with food and isnt eating
+        if(collision.gameObject.tag == "food" && !isEating) {
+            Debug.Log("Food registered");
+            isEating = true;
+            Destroy(collision.gameObject);
+            gameObject.GetComponent<BigFishColor>().FoodUP();
+            StartCoroutine(StopEating());
+        }
+    }
+
+    IEnumerator StopEating() {
+        yield return new WaitForSeconds(2);
+        Debug.Log("Done Eating");
+        isEating = false;
+    }
+}
---
+++ PropaFish/Assets/Scripts/BigFishColor.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigFishColor : MonoBehaviour {
+
+    Renderer rend;
+    Shader shader;
+
+    GameObject body;
+
+    int colorLevel;
+
+
+	// Use this for initialization
+	void Start () {
+        body = transform.Find("Body").gameObject;
+        rend = body.GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Standard");
+        colorLevel = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("v")) {
+            FoodUP();
+        }
+        if (Input.GetKeyDown("b")) {
+            FoodDown();
+        }
+    }
+
+    public void FoodUP() {
+        //Take the level, and make it go up
+        switch (colorLevel) {
+            case 0:
+                rend.material.color = new Color(.2f, 0, 0, 1);
+                transform.localScale += new Vector3(.5f, .5f, .5f);
+                colorLevel++;
+                break;
+            case 1:
+                rend.material.color = new Color(.4f, 0, 0, 1);
+                transform.localScale += new Vector3(.5f, .5f, .5f);
+                colorLevel++;
+                break;
+            case 2:
+                rend.material.color = new Color(.6f, 0, 0, 1);
+                transform.localScale += new Vector3(.5f, .5f, .5f);
+                colorLevel++;
+                break;
+            case 3:
+                rend.material.color = new Color(.8f, 0, 0, 1);
+                transform.localScale += new Vector3(.5f, .5f, .5f);
+                colorLevel++;
+                break;
+            case 4:
+                rend.material.color = new Color(1, 0, 0, 1);
+                colorLevel++;
+                break;
+            case 5:
+                Debug.Log("End of the line");
+                break;
+        } 
+    }
+
+    public void FoodDown() {
+
+        //Take the level, and make it go up
+        switch (colorLevel) {
+            case 0:
+                Debug.Log("Lowest point");
+                break;
+            case 1:
+                rend.material.color = new Color(0, 0, 0, 1);
+                colorLevel--;
+                break;
+            case 2:
+                rend.material.color = new Color(.2f, 0, 0, 1);
+                transform.localScale -= new Vector3(.5f, .5f, .5f);
+                colorLevel--;
+                break;
+            case 3:
+                rend.material.color = new Color(.4f, 0, 0, 1);
+                transform.localScale -= new Vector3(.5f, .5f, .5f);
+                colorLevel--;
+                break;
+            case 4:
+                rend.material.color = new Color(.6f, 0, 0, 1);
+                transform.localScale -= new Vector3(.5f, .5f, .5f);
+                colorLevel--;
+                break;
+            case 5:
+                rend.material.color = new Color(.8f, 0, 0, 1);
+                transform.localScale -= new Vector3(.5f, .5f, .5f);
+                colorLevel--;
+                break;
+        }
+    }
+}
