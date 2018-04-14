4c4eebd22df7bc995fdc9a49c6276132858b5a36
Du Lin <lnidux@gmail.com>
2018-04-14 12:37:31 +0200

    wip

--- PropaFish/Assets/Scripts/FishBehaviour.cs
+++ PropaFish/Assets/Scripts/FishBehaviour.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FishBehaviour : MonoBehaviour {
+	
+	[Range(0f, 5f)]
+	public float speed;
+	private Vector3 currentAngle;
+	private Vector3 targetAngle = new Vector3(90f, 0f, 0f);
 
-	// Use this for initialization
+	private bool moving;
 	void Start () {
-		
+		currentAngle = transform.eulerAngles;
+		moving = true;	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		Move();	
+	}
+
+	void Turn() {
+		Vector3 currentAngle = transform.eulerAngles;
+
+        transform.eulerAngles = new Vector3(
+            Mathf.LerpAngle(currentAngle.x, targetAngle.x, Time.deltaTime),
+            Mathf.LerpAngle(currentAngle.y, targetAngle.y, Time.deltaTime),
+            Mathf.LerpAngle(currentAngle.z, targetAngle.z, Time.deltaTime));
+		moving = true;
+    }
+
+	void Move() {
+		if(moving)
+        	transform.position = transform.position + speed * transform.right * Time.fixedDeltaTime;
+	}
+
+	void OnCollisionEnter(Collision collision)
+    {
+		Debug.Log("collision");
+		moving = false;
+        Turn();
 	}
 }
